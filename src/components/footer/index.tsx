5d0335e57a5038615cddb1ac05c27789a02d28a1
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-04-12 17:57:21 +0200

    wd

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="border-2 border-t border-secondary py-4">
+      <div className=" container">Footer</div>
+    </footer>
+  );
+}
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,31 @@
+import Button, { cn, buttonVariants } from "@/ui/button";
+import Link from "next/link";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <nav className="w-[100vw] bg-background fixed top-0 h-[70px] border-b-2 border-secondary flex  items-center">
+      <div className="flex container justify-between">
+        <div className="flex gap-6">
+          <h1 className="text-2xl font-bold">
+            <Link href="/">Webdrei UI</Link>
+          </h1>
+          <ul className="flex  items-center">
+            <li>
+              <Link
+                href="/components"
+                className={cn(buttonVariants({ variant: "link" }))}
+              >
+                Components
+              </Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <Button>Get Started</Button>
+        </div>
+      </div>
+    </nav>
+  );
+}
