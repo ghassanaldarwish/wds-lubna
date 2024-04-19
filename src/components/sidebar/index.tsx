07980d54b10e9860748dd7e8b3e618c4165cbae5
Ghassanooooo <ghassanaldarwish@yahoo.com>
2024-04-19 16:32:09 +0200

    wd

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -8,24 +8,38 @@ import { usePathname } from "next/navigation";
 import sidebar from "@/config/sidebar";
 import { cn } from "@/lib/utils";
 export default function Sidebar() {
+  const listRef = React.useRef<HTMLUListElement>(null);
   const pathname = usePathname();
-
   const sidebarData = Object.values(sidebar);
+  useEffect(() => {
+    if (!listRef.current) return;
+
+    const links = listRef.current.querySelectorAll("li[data-path]");
+    links.forEach((link) => {
+      if (link.getAttribute("data-path") === pathname) {
+        link.scrollIntoView({ behavior: "smooth", block: "center" });
+      }
+    });
+  }, [pathname]);
+
   return (
     <aside className="w-[300px] z-0  py-4  h-[100vh] sticky top-[70px]">
       <ScrollArea className="w-full h-full ">
         {sidebarData.map((item) => (
           <div className="mb-4" key={item.title}>
             <h2 className="font-bold mb-2">{item.title}</h2>
-            <motion.ul className="">
+            <motion.ul layout className="" ref={listRef}>
               {item.navigator.map((child) => (
                 <motion.li
+                  data-path={`/${child.path}`}
                   animate={{
                     fontWeight: pathname === `/${child.path}` ? "bold" : "",
                   }}
                   key={child.path}
                 >
-                  <Link href={`/${child.path}`}>{child.title}</Link>
+                  <Link className="hover:underline" href={`/${child.path}`}>
+                    {child.title}
+                  </Link>
                 </motion.li>
               ))}
             </motion.ul>
